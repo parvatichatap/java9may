93e77c2dbe0626505a96fb25033bfd840a103caf
parvatchatap <parvatichatap3@gmail.com>
2023-05-17 22:28:13 +0530

    Features InternalWorking JVM Architecture

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,5 +1,13 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word..........................!");
-    }
-}
+
+//        public class NumberPrinter {
+//            public static void main(String[] args) {
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+
